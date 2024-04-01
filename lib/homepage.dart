0f21c89904fc30aab3d83c0dbdb86a7bfc657224
Osamah <osamah.saddeq@gmail.com>
2024-04-01 08:06:21 +0300

    Created Categories Row

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -17,7 +17,7 @@ class _HomepageState extends State<Homepage> {
         children: [
           Row(
             children: [
-              const SizedBox(width: 8),
+              // const SizedBox(width: 8),
               Expanded(
                   child: TextFormField(
                 decoration: InputDecoration(
@@ -35,6 +35,93 @@ class _HomepageState extends State<Homepage> {
                   Icons.menu,
                   size: 37,
                 ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Categories",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(100)),
+                    child: Icon(
+                      Icons.laptop,
+                      size: 50,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text("Laptop")
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(100)),
+                    child: Icon(
+                      Icons.menu_book_rounded,
+                      size: 50,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text("Book")
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(100)),
+                    child: Icon(
+                      Icons.toys,
+                      size: 50,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text("Toys")
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(100)),
+                    child: Icon(
+                      Icons.electrical_services,
+                      size: 50,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text("electricity")
+                ],
               )
             ],
           )
